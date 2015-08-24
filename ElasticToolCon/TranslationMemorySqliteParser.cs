0de6c18e64adcc93aaed103fad1003b40db3e114
cchen <chenchen@gmail.com>
2015-08-24 15:47:24 -0700

    read directly from sdltm file

---
+++ ElasticToolCon/TranslationMemorySqliteParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ElasticToolCon
+{
+    public class TranslationMemorySqliteParser
+    {
+        private string sourcePath;
+        public TranslationMemorySqliteParser(string path)
+        {
+            sourcePath = path;
+        }
+
+        public IEnumerable<TranslationMemory> Parse()
+        {
+            List<TranslationMemory> result = new List<TranslationMemory>();
+            string connstr = @"data source=C:\Users\chenchen\Downloads\NW_MECHANICS.sdltm; Version=3;";
+            using (var m_dbConnection = new SQLiteConnection(connstr))
+            {
+                m_dbConnection.Open();
+                string sql = "select * from translation_units";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                { 
+                    var tm = ReadTranslationMemory(reader);
+                    result.Add(tm);
+                }
+            }
+            return result;
+        }
+
+        private TranslationMemory ReadTranslationMemory(SQLiteDataReader reader)
+        {
+            TranslationMemory tm = new TranslationMemory();
+            tm.id = reader.GetInt32(0); // id
+            tm.chinese = GetSegmentFromXml(reader.GetString(4)); // source_segment
+            tm.japanese = GetSegmentFromXml(reader.GetString(6)); // target_segment
+            tm.tags = reader.GetString(9) + reader.GetString(8); // user + date
+            return tm;
+        }
+
+        private string GetSegmentFromXml(string content)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Segment));
+            Segment xmlData = (Segment)serializer.Deserialize(new StringReader(content));
+            return xmlData.Elements[0].Value;
+        }
+    }
+}
---
+++ ElasticTools/TranlationMemorySqliteParserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ElasticToolCon;
+
+namespace ElasticTools
+{
+    [TestFixture]
+    public class TranlationMemorySqliteParserTest
+    {
+        [Test]
+        public void SqliteOpen()
+        {
+            string path = @"‪C:\Users\chenchen\Downloads\NW_MECHANICS.sdltm";
+            TranslationMemorySqliteParser e = new TranslationMemorySqliteParser(path);
+            e.Parse();
+        }
+    }
+}
