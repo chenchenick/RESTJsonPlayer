037ac5e725b98eea1492e8b58e2a33cb3fcaec9d
cchen <chenchen@gmail.com>
2015-08-25 09:21:12 -0700

    add untranked file

---
+++ ElasticToolCon/TranslationMemory.cs
@@ -0,0 +1,23 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticToolCon
+{
+    [ElasticType(
+        Name = "translationmemory", IdProperty = "id")] 
+    public class TranslationMemory
+    {
+        [ElasticProperty(Type = FieldType.Integer)]
+        public int id { get; set; }
+        [ElasticProperty(Type = FieldType.String, Analyzer = "jpanalyzer")]
+        public string japanese { get; set; }
+        [ElasticProperty(Type = FieldType.String, Analyzer = "cnanalyzer")]
+        public string chinese { get; set; }
+        [ElasticProperty(Type = FieldType.String)]
+        public string tags { get; set; }
+    }
+}
